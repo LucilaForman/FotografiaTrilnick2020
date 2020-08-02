62cd6668671c5af2da8413a2bee07c4315777b12
Tomas <tomas_forman@outlook.com>
2020-08-02 02:27:59 -0300

    Updates

---
+++ precache-manifest.3df04e200c7cf870edb680a77cc21c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c1ee29fed18c002cc409376f48e03",
+    "url": "/FotografiaTrilnick2020/index.html"
+  },
+  {
+    "revision": "8fcd21e055a9a0eee83a",
+    "url": "/FotografiaTrilnick2020/static/css/main.ebd04a46.chunk.css"
+  },
+  {
+    "revision": "a18e2725e9d81b0eb11f",
+    "url": "/FotografiaTrilnick2020/static/js/2.e8c5b5c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FotografiaTrilnick2020/static/js/2.e8c5b5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fcd21e055a9a0eee83a",
+    "url": "/FotografiaTrilnick2020/static/js/main.d6131e1e.chunk.js"
+  },
+  {
+    "revision": "738564cc4b7471438a4d",
+    "url": "/FotografiaTrilnick2020/static/js/runtime-main.48d32645.js"
+  }
+]);
